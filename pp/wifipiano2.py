f0e0367821a0c5b2d640b748c2c571132613b636
vipsuosu <s.vipsu@gmail.com>
2018-08-18 16:40:46 +0200

    Mania PP

--- pp/wifipiano2.py
+++ pp/wifipiano2.py
@@ -1,6 +1,5 @@
 """
 Wifipiano 2
-
 This file has been written taking by reference code from
 osu-performance (https://github.com/ppy/osu-performance)
 by Tom94, licensed under the GNU AGPL 3 License.
@@ -11,6 +10,8 @@ from constants import exceptions
 
 
 class piano:
+	__slots__ = ["beatmap", "score", "pp"]
+
 	def __init__(self, __beatmap, __score):
 		self.beatmap = __beatmap
 		self.score = __score
@@ -35,17 +36,19 @@ class piano:
 			# ---------- STRAIN PP
 			# Scale score to mods multiplier
 			scoreMultiplier = 1.0
-			if scoreMods & mods.EASY > 0:
-				scoreMultiplier *= 2.00
-			if scoreMods & mods.NOFAIL > 0:
-				scoreMultiplier *= 2.00
-			# NOTE: HT gives less pp tho
-			#if scoreMods & mods.HALFTIME > 0:
-			#	scoreMultiplier *= 2.00
+
+			# Doubles score if EZ/HT
+			if scoreMods & mods.EASY != 0:
+				scoreMultiplier *= 0.50
+				
+			if scoreMods & mods.HALFTIME != 0:
+				scoreMultiplier *= 0.50
+
+			# Calculate strain PP
 			if scoreMultiplier <= 0:
 				strainPP = 0
 			else:
-				score *= int(1.0/scoreMultiplier)
+				score *= int(1.0 / scoreMultiplier)
 				strainPP = pow(5.0 * max(1.0, stars / 0.0825) - 4.0, 3.0) / 110000.0
 				strainPP *= 1 + 0.1 * min(1.0, float(objects) / 1500.0)
 				if score <= 500000:
@@ -62,20 +65,48 @@ class piano:
 					strainPP *= 0.95 + float(score - 900000) / 100000.0 * 0.05
 
 			# ---------- ACC PP
-			hitWindow300 = 64-3*float(od)
+			# Makes sure OD is in range 0-10. If this is done elsewhere, remove this.
+			scrubbedOD = min(10.0, max(0, 10.0 - od))
+
+			# Old formula but done backwards.
+			hitWindow300 = (34 + 3 * scrubbedOD)
+
+			# Increases hitWindow if EZ is on
+			if scoreMods & mods.EASY != 0:
+				hitWindow300 *= 1.4
+
+			# Fiddles with DT and HT to make them match hitWindow300's ingame.
+			if scoreMods & mods.DOUBLETIME != 0:
+				hitWindow300 *= 1.5
+			elif scoreMods & mods.HALFTIME != 0:
+				hitWindow300 *= 0.75
+
+			# makes hit window match what it is ingame.
+			hitWindow300 = int(hitWindow300) + 0.5
+			if scoreMods & mods.DOUBLETIME != 0:
+				hitWindow300 /= 1.5
+			elif scoreMods & mods.HALFTIME != 0:
+				hitWindow300 /= 0.75
+
+			# Calculate accuracy PP
 			accPP = pow((150.0 / hitWindow300) * pow(accuracy, 16), 1.8) * 2.5
 			accPP *= min(1.15, pow(float(objects) / 1500.0, 0.3))
 
 			# ---------- TOTAL PP
 			multiplier = 1.1
-			if scoreMods & mods.NOFAIL > 0:
+			if scoreMods & mods.NOFAIL != 0:
 				multiplier *= 0.90
-			if scoreMods & mods.SPUNOUT > 0:
+			if scoreMods & mods.SPUNOUT != 0:
 				multiplier *= 0.95
-			if scoreMods & mods.EASY > 0:
+			if scoreMods & mods.EASY != 0:
+				multiplier *= 0.50
+			if scoreMods & mods.DOUBLETIME != 0:
+				multiplier *= 2.45
+			if scoreMods & mods.HALFTIME != 0:
 				multiplier *= 0.50
 			pp = pow(pow(strainPP, 1.1) + pow(accPP, 1.1), 1.0 / 1.1) * multiplier
 			log.debug("[WIFIPIANO2] Calculated PP: {}".format(pp))
+
 			self.pp = pp
 		except exceptions.invalidBeatmapException:
 			log.warning("Invalid beatmap {}".format(self.beatmap.beatmapID))
