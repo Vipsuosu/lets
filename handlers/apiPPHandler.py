8b046b0c0223b3aa97fcba8d20f6b15d1faaa8bd
vipsuosu <s.vipsu@gmail.com>
2018-08-18 16:41:15 +0200

    Remove Omppc

--- handlers/apiPPHandler.py
+++ handlers/apiPPHandler.py
@@ -1,21 +1,19 @@
 import json
 import sys
 import traceback
-import math
 
 import tornado.gen
 import tornado.web
 from raven.contrib.tornado import SentryMixin
 
 from objects import beatmap
-from common.constants import mods
 from common.constants import gameModes
 from common.log import logUtils as log
 from common.web import requestsManager
 from constants import exceptions
+from helpers import osuapiHelper
 from objects import glob
 from pp import rippoppai
-from pp import omppcPy
 from common.sentry import sentry
 
 MODULE_NAME = "api/pp"
@@ -23,7 +21,6 @@ class handler(requestsManager.asyncRequestHandler):
 	"""
 	Handler for /api/v1/pp
 	"""
-	@tornado.gen.coroutine
 	@tornado.web.asynchronous
 	@tornado.gen.engine
 	@sentry.captureTornado
@@ -63,111 +60,70 @@ class handler(requestsManager.asyncRequestHandler):
 				accuracy = self.get_argument("a")
 				try:
 					accuracy = float(accuracy)
-					if math.isnan(accuracy):
-						accuracy = -1.0
 				except ValueError:
 					raise exceptions.invalidArgumentsException(MODULE_NAME)
 			else:
 				accuracy = -1.0
-
-			if "x" in self.request.arguments:
-				misses = self.get_argument("x")
-				try:
-					misses = int(misses)
-				except ValueError:
-					raise exceptions.invalidArgumentsException(MODULE_NAME)
-			else:
-				misses = 0
-
-			if "c" in self.request.arguments:
-				combo = self.get_argument("c")
-				if not combo.isdigit():
-					raise exceptions.invalidArgumentsException(MODULE_NAME)
-				combo = int(combo)
-			else:
-				combo = 0
-
 
 			# Print message
 			log.info("Requested pp for beatmap {}".format(beatmapID))
 
 			# Get beatmap md5 from osuapi
 			# TODO: Move this to beatmap object
-			"""osuapiData = osuapiHelper.osuApiRequest("get_beatmaps", "b={}".format(beatmapID))
+			osuapiData = osuapiHelper.osuApiRequest("get_beatmaps", "b={}".format(beatmapID))
 			if osuapiData is None or "file_md5" not in osuapiData or "beatmapset_id" not in osuapiData:
 				raise exceptions.invalidBeatmapException(MODULE_NAME)
 			beatmapMd5 = osuapiData["file_md5"]
-			beatmapSetID = osuapiData["beatmapset_id"]"""
-			dbData = glob.db.fetch("SELECT beatmap_md5, beatmapset_id, mode FROM beatmaps WHERE beatmap_id = {}".format(beatmapID))
-			if dbData is None:
-				raise exceptions.invalidBeatmapException(MODULE_NAME)
-
-			beatmapMd5 = dbData["beatmap_md5"]
-			beatmapSetID = dbData["beatmapset_id"]
+			beatmapSetID = osuapiData["beatmapset_id"]
 
 			# Create beatmap object
 			bmap = beatmap.beatmap(beatmapMd5, beatmapSetID)
-			stars = 0
+
 			# Check beatmap length
 			if bmap.hitLength > 900:
 				raise exceptions.beatmapTooLongException(MODULE_NAME)
 
 			returnPP = []
 			if gameMode == gameModes.STD and bmap.starsStd == 0:
-				gameMode = dbData["mode"]
-				if(gameMode == 2):
-					raise exceptions.unsupportedGameModeException
-			if accuracy > 100 or combo > bmap.maxCombo or misses < 0 or math.isnan(accuracy):
-				raise exceptions.invalidArgumentsException(MODULE_NAME)
+				# Mode Specific beatmap, auto detect game mode
+				if bmap.starsTaiko > 0:
+					gameMode = gameModes.TAIKO
+				if bmap.starsCtb > 0:
+					gameMode = gameModes.CTB
+				if bmap.starsMania > 0:
+					gameMode = gameModes.MANIA
+
 			# Calculate pp
-			if gameMode != 2:
+			if gameMode == gameModes.STD:
 				# Std pp
-				if accuracy < 0 and combo == 0 and misses == 0 and gameMode == dbData["mode"]:
+				if accuracy < 0 and modsEnum == 0:
 					# Generic acc
 					# Get cached pp values
 					cachedPP = bmap.getCachedTillerinoPP()
-					if(modsEnum == 0 and cachedPP != [0,0,0,0]):
+					if cachedPP != [0,0,0,0]:
 						log.debug("Got cached pp.")
 						returnPP = cachedPP
-						stars = bmap.starsStd
 					else:
 						log.debug("Cached pp not found. Calculating pp with oppai...")
 						# Cached pp not found, calculate them
-						if(gameMode == 1 or gameMode == 0):
-							oppai = rippoppai.oppai(bmap, mods=modsEnum, tillerino=True, stars=True)
-							returnPP = oppai.pp
-							stars = oppai.stars
-						else:
-							xeno = omppcPy.piano(bmap, mods=modsEnum, tillerino=True, stars=True)
-							returnPP = xeno.pp
-							stars = xeno.stars
+						oppai = rippoppai.oppai(bmap, mods=modsEnum, tillerino=True, stars=True)
+						returnPP = oppai.pp
+						bmap.stars = oppai.stars
 
 						# Cache values in DB
 						log.debug("Saving cached pp...")
-						if(modsEnum == 0 and type(returnPP) != int and len(returnPP) == 4):
+						if len(returnPP) == 4:
 							bmap.saveCachedTillerinoPP(returnPP)
 				else:
 					# Specific accuracy, calculate
 					# Create oppai instance
-					if(gameMode == 3):
-							log.info("Specific request ({}%/{}). Calculating pp with omppc...".format(accuracy, modsEnum))
-							xeno = omppcPy.piano(bmap, acc=accuracy, mods=modsEnum, tillerino=False, stars=True)
-							returnPP.append(xeno.pp)
-							stars = xeno.stars	
+					log.debug("Specific request ({}%/{}). Calculating pp with oppai...".format(accuracy, modsEnum))
+					oppai = rippoppai.oppai(bmap, mods=modsEnum, tillerino=True, stars=True)
+					bmap.starsStd = oppai.stars
+					if accuracy > 0:
+						returnPP.append(calculatePPFromAcc(oppai, accuracy))
 					else:
-						if(gameMode != 0 and gameMode != 1):
-							raise exceptions.unsupportedGameModeException
-						log.debug("Specific request ({}%/{}). Calculating pp with oppai...".format(accuracy, modsEnum))
-						oppai = rippoppai.oppai(bmap, mods=modsEnum, tillerino=False, stars=True)
-						stars = oppai.stars
-						if accuracy > 0:
-							oppai.acc = accuracy
-						if combo > 0:
-							oppai.combo = combo
-						if misses > 0:
-							oppai.misses = misses	
-						oppai.getPP()
-						returnPP.append(oppai.pp)
+						returnPP = oppai.pp
 			else:
 				raise exceptions.unsupportedGameModeException
 
@@ -175,12 +131,13 @@ class handler(requestsManager.asyncRequestHandler):
 			data = {
 				"song_name": bmap.songName,
 				"pp": returnPP,
-				"length": round(bmap.hitLength*0.75) if modsEnum & mods.DOUBLETIME > 0 else bmap.hitLength if modsEnum & mods.HALFTIME < 1 else round(bmap.hitLength*1.50),
-				"stars": stars,
-				"ar": round(min(10, bmap.AR * 1.4),1) if modsEnum & mods.HARDROCK > 0 else bmap.AR if modsEnum & mods.EASY < 1 else round(max(0, bmap.AR / 2),1),
-				"od": round(min(10, bmap.OD * 1.4),1) if modsEnum & mods.HARDROCK > 0 else bmap.OD if modsEnum & mods.EASY < 1 else round(max(0, bmap.OD / 2),1),
-				"bpm": round(bmap.bpm*1.5) if modsEnum & mods.DOUBLETIME > 0 else bmap.bpm if modsEnum & mods.HALFTIME < 1 else round(bmap.bpm*0.75),
+				"length": bmap.hitLength,
+				"stars": bmap.starsStd,
+				"ar": bmap.AR,
+				"od": bmap.OD,
+				"bpm": bmap.bpm,
 			}
+
 			# Set status code and message
 			statusCode = 200
 			data["message"] = "ok"
@@ -209,3 +166,8 @@ class handler(requestsManager.asyncRequestHandler):
 			self.write(json.dumps(data))
 			self.set_header("Content-Type", "application/json")
 			self.set_status(statusCode)
+
+def calculatePPFromAcc(ppcalc, acc):
+	ppcalc.acc = acc
+	ppcalc.getPP()
+	return ppcalc.pp
